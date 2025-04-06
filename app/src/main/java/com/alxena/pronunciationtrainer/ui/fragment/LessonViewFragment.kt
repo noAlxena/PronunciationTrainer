66490572d7b6ffcb811b7588b4cb3855d01cf292
no_Alxena <masternik2003@gmail.com>
2025-04-07 02:10:30 +0300

    add: New fragments

--- app/src/main/java/com/alxena/pronunciationtrainer/ui/fragment/LessonViewFragment.kt
+++ app/src/main/java/com/alxena/pronunciationtrainer/ui/fragment/LessonViewFragment.kt
@@ -7,24 +7,23 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.alxena.pronunciationtrainer.R
-import com.alxena.pronunciationtrainer.databinding.FragmentAuthorsBinding
+import com.alxena.pronunciationtrainer.databinding.FragmentLessonListBinding
 
-class AuthorsFragment: Fragment() {
-    private var _binding: FragmentAuthorsBinding? = null
+class LessonViewFragment: Fragment() {
+    private var _binding: FragmentLessonListBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentAuthorsBinding.inflate(inflater, container, false)
+        _binding = FragmentLessonListBinding.inflate(inflater, container, false)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.backhome.setOnClickListener(){
-            findNavController().navigate(R.id.action_authorsFragment_to_startFragment)
-        }
+        //binding.backhome.setOnClickListener(){
+        //    findNavController().navigate(R.id.action_authorsFragment_to_startFragment)
+        //}
     }
     override fun onDestroyView() {
         super.onDestroyView()
